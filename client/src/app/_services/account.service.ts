cbfa46f83c68f22c54df20700b94fb3ec71bc7cc
Ashok Kumar Kottagundu <kak.mca@hotmail.com>
2023-08-06 07:17:59 +0530

    End of section 5, Angular update from v14 to v16

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -29,6 +29,17 @@ export class AccountService {
     );
   }
 
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
   /* This is just the convenient method to use this from our component */
   setCurrentUser(user: User) {
     this.currentUserSource.next(user);

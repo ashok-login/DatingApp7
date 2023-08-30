5d76726c9f6e40d4c96a2b62ccdafb83d4901e7e
Ashok Kumar Kottagundu <kak.mca@hotmail.com>
2023-08-30 17:06:25 +0530

    End of section 8

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -1,14 +1,16 @@
-using API.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class MemberDto
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public DateOnly DateOfBirth { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
@@ -18,11 +20,7 @@ namespace API.Entities
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public List<Photo> Photos { get; set; } = new();
-
-        public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }
+        public List<PhotoDto> Photos { get; set; }
+        
     }
 }
---
+++ API/Helpers/AutomapperProfiles.cs
@@ -0,0 +1,23 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutomapperProfiles : Profile
+    {
+        public AutomapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl,
+                    options => options.MapFrom(
+                        src => src.Photos
+                            .FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age,
+                    options => options.MapFrom(
+                        src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
